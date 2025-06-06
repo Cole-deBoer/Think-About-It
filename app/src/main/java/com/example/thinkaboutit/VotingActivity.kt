6047d8025d832a767ceaf8c0ed96d94f69838e8c
Cole deBoer <pg27Cole@vfs.com>
2025-06-06 13:20:34 -0700

    ADDED: creating vote cards at runtime for scalability

--- app/src/main/java/com/example/thinkaboutit/VotingActivity.kt
+++ app/src/main/java/com/example/thinkaboutit/VotingActivity.kt
@@ -3,18 +3,22 @@ package com.example.thinkaboutit
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Layout
+import android.text.Layout.Builder
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import com.google.firebase.database.values
 import kotlinx.coroutines.flow.first
 
 class VotingActivity : AppCompatActivity(), State {
-
-    lateinit var user1 : VoteCard;
-    lateinit var user2 : VoteCard;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,24 +60,22 @@ class VotingActivity : AppCompatActivity(), State {
         GameManager.Instance.currentState = this
         GameManager.Instance.queuedState = LeaderboardActivity()
 
-        // if we are the host set the images to be the submitted drawings
+        ServiceManager.Instance.usersRef.get().addOnSuccessListener { usersSnapshot ->
 
-        ServiceManager.Instance.hostRef.get().addOnSuccessListener {snapshot ->
-            //if(snapshot.value != ServiceManager.Instance.auth.currentUser?.uid) return@addOnSuccessListener
+            for(user in usersSnapshot.children)
+            {
+                //if(user.key == ServiceManager.Instance.auth.currentUser?.uid) continue
 
-            ServiceManager.Instance.usersRef.get().addOnSuccessListener { usersSnapshot ->
-                user1 = VoteCard(findViewById(R.id.drawingImage1), findViewById(R.id.voteButton1), usersSnapshot.children.first().key.toString())
-                user2 = VoteCard(findViewById(R.id.drawingImage2), findViewById(R.id.voteButton2), usersSnapshot.children.elementAt(1).key.toString())
+                val card = LayoutInflater.from(this).inflate(R.layout.user_vote_card, findViewById(R.id.verticalLayout_id), false)
 
-                ServiceManager.Instance.getUserImage(user1.userId) { bitmap ->
-                    user1.drawing.setImageBitmap(bitmap)
+
+                ServiceManager.Instance.getUserImage(user.key.toString()) { bitmap ->
+                    val userCard = VoteCard(card.findViewById(R.id.image_id), card.findViewById(R.id.voteButton_id), user.key.toString())
+
+                    userCard.drawing.setImageBitmap(bitmap)
+                    userCard.voteButton.setOnClickListener(vote(userCard.userId))
+                    findViewById<LinearLayout>(R.id.verticalLayout_id).addView(card)
                 }
-                ServiceManager.Instance.getUserImage(user2.userId) { bitmap ->
-                    user2.drawing.setImageBitmap(bitmap)
-                }
-
-                user1.voteButton.setOnClickListener(vote(user1.userId))
-                user2.voteButton.setOnClickListener(vote(user2.userId))
             }
         }
     }
